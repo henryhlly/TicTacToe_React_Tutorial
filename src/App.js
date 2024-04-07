f6ea847b4ea7fb3e94dfc17aefc3c5dab176d349
Henry Ly <lyhenry2004@gmail.com>
2024-04-07 16:09:43 +1200

    Finished the tutorial with no extras

--- src/App.js
+++ src/App.js
@@ -57,13 +57,21 @@ function Board({ xIsNext, squares, onPlay}) {
   return (
     <>
       <div className = "status">{status}</div>
-      for (let row = 0; row < 4; row++) {
-        <div className = "board-row">
-          for (let col = 0; col < 4; col++) {
-            <Square value = {squares[row+col]} onSquareClick={() => handleClick(row+col)} />
-          }
-        </div>
-      }
+      <div className = "board-row">
+        <Square value = {squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value = {squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value = {squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className = "board-row">
+        <Square value = {squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value = {squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value = {squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className = "board-row">
+        <Square value = {squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value = {squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value = {squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
   ) ;
 }
